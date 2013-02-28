eee051aff9c0029afb5d6a0326e6e2e31013a643
jcooky <bak723@gmail.com>
2013-02-28 14:32:34 +0900

    mina-thrift upgrade

--- src/main/java/com/github/karan/server/Server.java
+++ src/main/java/com/github/karan/server/Server.java
@@ -3,64 +3,74 @@ package com.github.karan.server;
 import com.github.jcooky.mina.thrift.TIoAcceptorServerTransport;
 import com.github.jcooky.mina.thrift.TIoSessionTransport;
 import com.github.jcooky.mina.thrift.TMinaServer;
+import com.github.jcooky.mina.thrift.codec.TFrameProtocolCodecFactory;
 import com.github.karan.server.gateway.GatewayImpl;
 import com.github.karan.server.gateway.gen.Gateway;
+
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.transport.TTransportFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 /**
- * Created with IntelliJ IDEA.
- * User: jcooky
- * Date: 13. 1. 16.
- * Time: 오전 11:18
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: jcooky Date: 13. 1. 16. Time: 오전 11:18 To
+ * change this template use File | Settings | File Templates.
  */
 public class Server {
-    private int port = 7900;
-    private TServer server;
-    private Gateway.Iface gateway ;
+	private int port = 7900;
+	private TServer server;
+	private Gateway.Iface gateway;
 
-    public Server() {
-        gateway = new GatewayImpl();
-    }
+	public Server() {
+		gateway = new GatewayImpl();
+	}
 
-    public void setGateway(Gateway.Iface gateway) {
-        this.gateway = gateway;
-    }
+	public void setGateway(Gateway.Iface gateway) {
+		this.gateway = gateway;
+	}
 
-    public Gateway.Iface getGateway() {
-        return gateway;
-    }
+	public Gateway.Iface getGateway() {
+		return gateway;
+	}
 
-    public void setServer(TServer server) {
-        this.server = server;
-    }
+	public void setServer(TServer server) {
+		this.server = server;
+	}
 
-    public void setPort(int port) {
-        this.port = port;
-    }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    public void serve() throws IOException {
-        if (this.server == null) {
-            TIoAcceptorServerTransport socket = new TIoAcceptorServerTransport(port);
-            TProcessor processor = new Gateway.Processor <Gateway.Iface>(new GatewayImpl());
-            server = new TMinaServer(new TMinaServer.Args(socket)
-                    .processor(processor)
-                    .protocolFactory(new TCompactProtocol.Factory())
-                    .inputTransportFactory(new TIoSessionTransport.InputTransportFactory())
-                    .outputTransportFactory(new TTransportFactory()));
-        }
+	public void serve() throws IOException {
+		if (this.server == null) {
+			NioSocketAcceptor acceptor;
+			acceptor = new NioSocketAcceptor(Runtime.getRuntime().availableProcessors() + 1);
+			acceptor.setReuseAddress(true);
 
-        server.serve();
-    }
+			acceptor.setDefaultLocalAddress(new InetSocketAddress(port));
 
-    public void stop() {
-        if (server != null) {
-            server.stop();
-        }
-    }
+			acceptor.getFilterChain().addLast(TIoAcceptorServerTransport.CODEC_NAME,
+					new ProtocolCodecFilter(new TFrameProtocolCodecFactory()));
+
+			TIoAcceptorServerTransport socket = new TIoAcceptorServerTransport(acceptor);
+			TProcessor processor = new Gateway.Processor<Gateway.Iface>(new GatewayImpl());
+			server = new TMinaServer(new TMinaServer.Args(socket).processor(processor)
+					.protocolFactory(new TCompactProtocol.Factory())
+					.inputTransportFactory(new TTransportFactory())
+					.outputTransportFactory(new TTransportFactory()));
+		}
+
+		server.serve();
+	}
+
+	public void stop() {
+		if (server != null) {
+			server.stop();
+		}
+	}
 }
--- src/test/java/com/github/karan/server/gateway/utils/TMinaUtils.java
+++ src/test/java/com/github/karan/server/gateway/utils/TMinaUtils.java
@@ -1,19 +1,33 @@
 package com.github.karan.server.gateway.utils;
 
+import java.net.InetSocketAddress;
+
 import com.github.jcooky.mina.thrift.TIoAcceptorServerTransport;
 import com.github.jcooky.mina.thrift.TIoSessionTransport;
 import com.github.jcooky.mina.thrift.TMinaServer;
+import com.github.jcooky.mina.thrift.codec.TFrameProtocolCodecFactory;
+
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.TTransportFactory;
 
 public class TMinaUtils {
 	public TMinaServer getServer(int port, TProcessor processor) {
-		TIoAcceptorServerTransport socket = new TIoAcceptorServerTransport(port);
-		return new TMinaServer(new TMinaServer.Args(socket)
-												.processor(processor)
-												.protocolFactory(new TBinaryProtocol.Factory())
-												.inputTransportFactory(new TIoSessionTransport.InputTransportFactory())
-												.outputTransportFactory(new TTransportFactory()));
+		NioSocketAcceptor acceptor;
+		acceptor = new NioSocketAcceptor(Runtime.getRuntime().availableProcessors() + 1);
+		acceptor.setReuseAddress(true);
+
+		acceptor.setDefaultLocalAddress(new InetSocketAddress(port));
+
+		acceptor.getFilterChain().addLast(TIoAcceptorServerTransport.CODEC_NAME,
+				new ProtocolCodecFilter(new TFrameProtocolCodecFactory()));
+
+		TIoAcceptorServerTransport socket = new TIoAcceptorServerTransport(acceptor);
+		return new TMinaServer(new TMinaServer.Args(socket).processor(processor)
+				.protocolFactory(new TBinaryProtocol.Factory())
+				.inputTransportFactory(new TTransportFactory())
+				.outputTransportFactory(new TTransportFactory()));
 	}
 }
